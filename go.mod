6b2f1f1be176a4a2ad8b5d2f690ac1dc38db1e82
Federico <federicocavallini100@gmail.com>
2023-12-10 19:05:54 +0100

    start new structure

--- go.mod
+++ go.mod
@@ -2,15 +2,4 @@ module type-racer
 
 go 1.21.3
 
-require github.com/rivo/tview v0.0.0-20231206124440-5f078138442e
-
-require (
-	github.com/gdamore/encoding v1.0.0 // indirect
-	github.com/gdamore/tcell/v2 v2.6.1-0.20231203215052-2917c3801e73 // indirect
-	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/mattn/go-runewidth v0.0.14 // indirect
-	github.com/rivo/uniseg v0.4.3 // indirect
-	golang.org/x/sys v0.11.0 // indirect
-	golang.org/x/term v0.9.0 // indirect
-	golang.org/x/text v0.12.0 // indirect
-)
+require github.com/go-chi/chi/v5 v5.0.10 // indirect
--- main.go
+++ main.go
@@ -1,43 +1,25 @@
 package main
 
 import (
-	"github.com/rivo/tview"
+	"fmt"
+	"net/http"
+	"text/template"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
 
-	newPrimitive := func(text string) tview.Primitive {
-		return tview.NewTextView().
-			SetTextAlign(tview.AlignCenter).
-			SetText(text)
-	}
+	PORT := ":3000"
+	router := chi.NewRouter()
 
-	grid := tview.NewGrid().
-		SetRows(10, 0, 10).
-		SetColumns(30, 0, 30).
-		SetBorders(true).
-		AddItem(newPrimitive("Quote"), 0, 0, 1, 3, 0, 0, false).
-		AddItem(newPrimitive("Type here"), 1, 0, 2, 3, 0, 0, false).
-		AddItem(newPrimitive("Stats"), 1, 0, 2, 3, 0, 0, false)
+	router.Get("/", indexHandler)
 
-	if err := tview.NewApplication().SetRoot(grid, true).SetFocus(grid).Run(); err != nil {
-		panic(err)
-	}
-	// app := tview.NewApplication()
+	fmt.Printf("Server listening on http://localhost%v", PORT)
+	http.ListenAndServe(PORT, router)
+}
 
-	// typeBox := tview.NewBox().SetBorder(true).SetBorderColor(1).SetTitle("Typing output")
-
-	// if err := app.SetRoot(typeBox, true).Run(); err != nil {
-	// 	panic(err)
-	// }
-
-	// button1 := tview.NewButton("Hit to stop!").SetSelectedFunc(func() {
-	// 	app.Stop()
-	// })
-
-	// button1.SetBorder(true).SetRect(0, 0, 22, 3)
-	// if err := app.SetRoot(button1, false).SetFocus(button1).Run(); err != nil {
-	// 	panic(err)
-	// }
-
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	templ := template.Must(template.ParseFiles("./template/index.html"))
+	templ.Execute(w, nil)
 }
