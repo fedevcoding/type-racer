1c5f8157a6a655d3733390c0a0b4cb26db73bff2
fedevcoding <fedevcoding@gmail.com>
2024-09-04 19:03:38 +0200

    update quote api url not working

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const HEIGHT = process.stdout.rows;
 export const WIDTH = process.stdout.columns;
-export const RANDOM_QUOTES_API = "https://api.quotable.io/quotes/random";
+export const RANDOM_QUOTES_API = "https://dummyjson.com/quotes/random";
--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,7 @@ import { RANDOM_QUOTES_API } from "./constants.js";
 import crypto from "crypto";
 
 export const fetchQuote = async (): Promise<string> => {
-  const quote = (await (await fetch(RANDOM_QUOTES_API)).json())?.[0]?.content;
+  const quote = (await (await fetch(RANDOM_QUOTES_API)).json())?.quote;
   return quote;
 };
 
